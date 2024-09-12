fc355b5005620db7722731b08e69eb767c96752a
EconLQ <zavliybogdan@gmail.com>
2024-09-12 12:29:45 +0300

    updated consumed media type on PUT request and error handler

--- src/main/java/com/gameshop/ecommerce/web/user/controller/AccountController.java
+++ src/main/java/com/gameshop/ecommerce/web/user/controller/AccountController.java
@@ -1,11 +1,13 @@
 package com.gameshop.ecommerce.web.user.controller;
 
+import com.gameshop.ecommerce.utils.MessageResponse;
 import com.gameshop.ecommerce.web.user.model.User;
 import com.gameshop.ecommerce.web.user.model.UserDTO;
 import com.gameshop.ecommerce.web.user.model.UserInfoDTO;
 import com.gameshop.ecommerce.web.user.service.AccountService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -25,10 +27,14 @@ public class AccountController {
         return ResponseEntity.of(userDto);
     }
 
-    @PutMapping
-    public ResponseEntity<UserDTO> updateInfo(@AuthenticationPrincipal User user, @RequestBody UserDTO userDto) {
-        UserDTO updatedUser = accountService.updateInfo(user, userDto);
-        return ResponseEntity.ok(updatedUser);
+    @PutMapping(consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public ResponseEntity<?> updateInfo(@AuthenticationPrincipal User user, @ModelAttribute UserDTO userDto) {
+        UserDTO updatedUser = null;
+        try {
+            updatedUser = accountService.updateInfo(user, userDto);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(new MessageResponse(e.getMessage()));
+        }
+        return ResponseEntity.ok().body(updatedUser);
     }
-
 }
