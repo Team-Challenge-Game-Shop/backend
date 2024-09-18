eb847fd4e6b00e5cc31a7803f82bf1a56664fede
EconLQ <zavliybogdan@gmail.com>
2024-09-19 00:04:45 +0300

    changed to get mapping

--- src/main/java/com/gameshop/ecommerce/security/auth/controller/AuthenticationController.java
+++ src/main/java/com/gameshop/ecommerce/security/auth/controller/AuthenticationController.java
@@ -49,7 +49,7 @@ public class AuthenticationController {
         return ResponseEntity.ok(response);
     }
 
-    @PostMapping(value = "/verify")
+    @GetMapping(value = "/verify")
     public ResponseEntity<?> verifyEmail(@RequestParam("token") String confirmationCode) {
         try {
             final LoginResponse loginResponse = authenticationService.verifyEmail(confirmationCode);
